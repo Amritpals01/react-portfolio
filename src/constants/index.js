d91edb4038dbb53d70baab40fe41fa08d177c3cc
Amritpals01 <amritpal15k@gmail.com>
2024-10-14 01:14:35 +0530

    ok

--- src/constants/index.js
+++ src/constants/index.js
@@ -65,6 +65,6 @@ export const PROJECTS = [
 
 export const CONTACT = {
   address: "16 Aman avenue , Majitha road ,ASR",
-  phoneNo: "+91 628310422 ",
+  phoneNo: "+91 6283104223 ",
   email: "amritpal15k@gmail.com",
 };
